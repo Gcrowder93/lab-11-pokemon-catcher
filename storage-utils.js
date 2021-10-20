a6e86ba854114316a0cf4ee6f00d92b8c9048d1f
Chase Crowder <gcrowder93@gmail.com>
2021-10-20 16:13:45 -0700

    fixed local storage

--- storage-utils.js
+++ storage-utils.js
@@ -1,4 +1,6 @@
-export function findById(id, pokes){
+
+
+export function findByPokemon(pokemon, pokes){
 
     for (let poke of pokes) {
         if (poke.id === id){
@@ -20,4 +22,38 @@ export function getPoke(){
     }
     return pokes || pokemon;
 
+}
+
+export function encounter(pokemon){
+    let pokeArray = getPoke();
+    let foundPoke = findByPokemon(pokemon, pokeArray);
+
+
+    if (foundPoke){
+        foundPoke.encounter++;
+       // const pokeString = JSON.stringify(pokemon);
+       // localStorage.setItem('POKEMON', pokeString);
+    } else {
+        const newPoke = {
+            pokemon: pokemon.pokemon,
+            encounted: 1,
+            captured: 0,
+        };
+        pokeArray.push(newPoke);
+    }
+    const arrayString = JSON.stringify(pokeArray);
+    localStorage.setItem('POKEMON', arrayString);
+    return pokeArray;
+}
+
+export function getResults(){
+    const resultString = localStorage.getItem('RESULT') || [];
+    const result = JSON.parse(resultString);
+    return result;
+}
+
+export function getChosen(){
+    const chosenString = localStorage.getItem('RESULT') || [];
+    const chosen = JSON.parse(chosenString);
+    return chosen;
 }
